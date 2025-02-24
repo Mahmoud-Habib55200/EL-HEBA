29cd79fc06284da6f0ae904463db1ece41909b0b
Mahmoud Habib <mahmoudhabib55200@gmail.com>
2025-02-24 15:41:38 +0200

    Resize Images

--- src/components/WhyHeba.jsx
+++ src/components/WhyHeba.jsx
@@ -5,50 +5,50 @@ const data = [
   {
     title:
       "34,000 acres of citrus nurseries with advanced irrigation and fertilization technology.",
-    // image: why1,
+    image: why1,
     desc: "The 34,000-acre citrus nurseries use advanced irrigation and fertilization, ensuring healthier crops, efficient resources, and sustainable practices.",
   },
   {
     title: "World-class citrus production and export facilities.",
-    // image: why2,
+    image: why2,
     desc: "Featuring world-class citrus production and export facilities, these operations ensure high-quality fruit, efficient logistics, and global market reach.",
   },
   {
     title: "Commitment to sustainability and eco-friendly practices.",
-    // image: why3,
+    image: why3,
     desc: "Dedicated to sustainability, these facilities prioritize eco-friendly practices, promoting environmental conservation and responsible resource management for a greener future.",
   },
   {
     title: "Pioneers in agricultural innovation and quality.",
-    // image: why4,
+    image: why4,
     desc: "Leading in agricultural innovation, they prioritize quality, utilizing advanced techniques to set new standards in farming and productivity.",
   },
   {
     title: "Expansive supply chain network for global reach.",
-    // image: compony,
+    image: compony,
     desc: "With an expansive supply chain network, they ensure seamless distribution, enabling efficient global reach and timely delivery of products.",
   },
   {
     title: "Continuous innovation for superior products.",
-    // image: finishMontg,
+    image: finishMontg,
     desc: "Driven by continuous innovation, they consistently deliver superior products, meeting evolving market demands with excellence and quality.",
   },
 ];
 
 // Card Component
-const WhyCard = memo(({ title,  desc }) => {
+const WhyCard = memo(({ title, image,  desc }) => {
   return (
     <div
       className="bg-white border border-gray-200 rounded-xl shadow-md overflow-hidden 
              hover:shadow-lg transition-transform transform "
     >
       <div className="relative overflow-hidden">
-        {/* <img
+        <img
           src={image}
           alt={title}
           loading="lazy" // Lazy Loading for better performance
           className="w-full h-48 object-cover transition-transform duration-300 hover:scale-105"
-        /> */}
+        />
       </div>
       <div className="p-6">
         <h3 className="text-lg font-bold text-main_Color mb-2 capitalize">
